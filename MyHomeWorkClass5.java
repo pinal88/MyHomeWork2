1ea544462bfc4629b870c588c1f86068fac4f59b
Pinal Pema <pinal88pema@gmail.com>
2023-10-31 22:31:09 +0000

    new file

---
+++ MyHomeWorkClass5.java
@@ -0,0 +1,61 @@
+package myhomeworkpackage2;
+
+public class MyHomeWorkClass5 {
+    public static void main(String[] args) {
+        //integer family
+        //byte range is-128 to 127
+        byte g = -120;
+        System.out.println("g="+g);
+
+        //short range is -32768 to 32767
+        short h = 31768;
+        System.out.println("h="+h);
+
+
+        //int range is -2147483648 to 2147483647
+        int i = 2147483646;
+        System.out.println("i="+i);
+
+        //long range is -9223372036854775808 to -9223372036854775807
+        long j = 9223372036854775807L;
+        System.out.println("j="+j);
+
+
+        //float family
+        //float range is 3.4E-38 to 3.4E+38
+        float k = 46466644484812411114444.11111111111111111111111111111f;
+        System.out.println("k="+k);
+
+        //Double range is approximately 1.8*10(308)
+        double l = 11111111222222222222.222222222222222222222222222222222d;
+        System.out.println("l="+l);
+
+
+        //Character family
+        //Character range is unicode value (single range value)
+        char m = '7';
+        System.out.println("m="+m);
+
+
+        //boolean family
+        //boolean don't have range . it's a fix value as true or false
+        boolean n = false;
+        System.out.println("n="+n);
+
+        //arithmetic Operators
+        int o=50;
+        int p=90;
+        //addition
+        System.out.println(o+p);// =50+90=140
+        //subtraction
+        System.out.println(o-p);// =50-90=-40
+        //multiplication
+        System.out.println(o*p);// =50*90=4500
+        //division
+        System.out.println(o/p);// =50/90=0
+        //modulus
+        System.out.println(o%p);// =50%90=50
+
+
+    }
+}
---
+++ MyHomeWorkClass6.java
@@ -0,0 +1,37 @@
+package myhomeworkpackage2;
+
+public class MyHomeWorkClass6 {
+    public static void main(String[] args) {
+        int q=60;
+        int r=20;
+        //addition
+        System.out.println(q+r); // =60+20 =80
+
+        //Subtraction
+        System.out.println(q-r); // =60-20 =40
+
+        //multiplication
+        System.out.println(q*r); // =60*20 =1200
+
+        //division
+        System.out.println(q/r); // =60/20 =3
+
+        //increment operator
+        System.out.println(q++); // 60 60+1 =60
+        System.out.println(++q); // 61+1 60 =62
+
+        System.out.println(r++); // 20 20+1 =20
+        System.out.println(++r); // 21+1 20 =22
+
+
+        //decrement operator
+        System.out.println(r--); // 22 22-1 =22
+        System.out.println(--r); // 21-1 20 =20
+
+        System.out.println(q--); // 62 62-1 =62
+        System.out.println(--q); // 61-1 60 =60
+
+
+
+    }
+}
